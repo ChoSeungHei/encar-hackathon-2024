f8a9a057ad18fb87a87529f771fa36db69156612
Youngsun21 <youngsun.park@carsales.com.au>
2024-03-06 14:15:46 +1100

    Added MUI and empty tab components

---
+++ src/Components/Tabs.tsx
@@ -0,0 +1,18 @@
+import Tab from '@mui/material/Tab';
+import React from 'react';
+
+interface ScrollableTabsProps {
+    
+}
+
+const ScrollableTabs: React.FC<ScrollableTabsProps> = ({
+
+}) => {
+    return (
+        <>
+        
+        </>    
+    );
+};
+
+export default ScrollableTabs;
