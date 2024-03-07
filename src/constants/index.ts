7016b714a79a962857d4569bf0b28feff76f5d8c
Ban younghun <40445771+youngrove@users.noreply.github.com>
2024-03-07 13:41:14 +0000

    pannel completed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,46 +1,3 @@
-export const INTERIOR = [
-    {
-        id:'Dashboard',
-        name:'Dashboard',
-        desc:'The speed system consists of speed-based, a coolant temperature system, fuel system, fuel-based, engine/ engine/ engine/ engine error warning.Usually, fuel shortage warning and battery warning lights are essential.',
-        img:'',
-        inspection_result:'',
-        
-    },
-    {
-        id:'Handle',
-        name:'Handle',
-        desc:'It refers to a molding device used when a car rotates. As it is a device that manipulates the movement of a vehicle, it directly affects the sense of operation depending on the shape and size of the vehicle',
-        img:'',
-        inspection_result:'',
-        
-    },
-    {
-        id:'CenterFascia',
-        name:'Center facia',
-        desc:"The center fascia is the panel between the driver's seat and the passenger seat in the dashboard. Various buttons are gathered, including audio, air conditioning, navigation, air outlet, cigar jack, and cup holder.",
-        img:'',
-        inspection_result:'The frequently used ventilation button appears to be a bit rough. You can easily replace the button cover by purchasing it separately for about 20,000 won.',
-        explain:'This is a stepping stone that allows easy entry and exit of tall vehicles such as SUVs. If a vehicle is used by many children and the elderly, it is better to have side steps.'
-    },
-    {
-        id:'ConsoleBox',
-        name:'ConsoleBox & Glove Box',
-        desc:"It is a box-shaped storage space installed in a car. The console between the driver's seat and the passenger seat in the front seat is called the center console, and the console located in the middle of the rear seat is called the rear console.",
-        img:'',
-        inspection_result:'',
-        
-    },
-    {
-        id:'Armrest',
-        name:'Armrest',
-        desc:'As a device, it means a part of a car to put your arm to put your arm to put your arm on the vehicle, or passengers can take a comfortable posture even in cars.',
-        img:'',
-        inspection_result:'',
-        
-    },
-]
-
 export const PARTS = {
     INTERIOR:[{
         id:'Dashboard',
@@ -71,7 +28,6 @@ export const PARTS = {
         desc:"It is a box-shaped storage space installed in a car. The console between the driver's seat and the passenger seat in the front seat is called the center console, and the console located in the middle of the rear seat is called the rear console.",
         img:'',
         inspection_result:'',
-        
     },
     {
         id:'Armrest',
@@ -79,7 +35,6 @@ export const PARTS = {
         desc:'As a device, it means a part of a car to put your arm to put your arm to put your arm on the vehicle, or passengers can take a comfortable posture even in cars.',
         img:'',
         inspection_result:'',
-        
     }],
     EXTERIOR:[
         {
@@ -101,7 +56,7 @@ export const PARTS = {
         id:'CarRadiator',
         name:'Car radiator',
         desc:'It serves to cool the engine that always gets hot while driving the car. Located at the front of the vehicle, it circulates air and lower heat from the engine.',
-        img:'/img/radiator.jpg',
+        img:'/img/radiator.png',
         inspection_result:'',
     },
     ]
