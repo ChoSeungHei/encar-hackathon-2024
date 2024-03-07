c11fca4763d9d5ab2d2f0b92615bff2bd1118e87
shcho <shcho@encar.com>
2024-03-07 21:08:23 +0900

    (fix) #Glossary 버튼 추가

--- src/Components/Controller/Controller.tsx
+++ src/Components/Controller/Controller.tsx
@@ -4,11 +4,14 @@ import './Controller.scss';
 function Controller() {
 	return(
 		<div className="controller_box">
+		<div className="arrow_box">
 			<button id="origin" className="controller_btn arrow origin">Interior View</button>
 			<button id="left" className="controller_btn arrow left">Left</button>
 			<button id="right" className="controller_btn arrow right">Right</button>
 			<button id="up" className="controller_btn arrow up">Up</button>
 			<button id="down" className="controller_btn arrow down">Down</button>
+		</div>
+			<button className="btn_open arrow">Glossary</button>
 		</div>
 	);
 }
